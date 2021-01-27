7ccf05c6167554cac321a6829ef379156e402748
Julim314 <72747996+Julim314@users.noreply.github.com>
2021-01-27 10:41:46 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 while True:
-    if input.rotation(Rotation.PITCH) > 2 and input.rotation(Rotation.ROLL) > 2:
+    if input.acceleration(Dimension.X) > 2 and input.acceleration(Dimension.Y) > 2 and input.acceleration(Dimension.Z) > 2:        
         light.set_all(color.rgb(250, 100, 55))
     else:
         light.set_all(light.rgb(0,0,0))
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 while (true) {
-    if (input.rotation(Rotation.Pitch) > 2 && input.rotation(Rotation.Roll) > 2) {
+    if (input.acceleration(Dimension.X) > 2 && input.acceleration(Dimension.Y) > 2 && input.acceleration(Dimension.Z) > 2) {
         light.setAll(color.rgb(250, 100, 55))
     } else {
         light.setAll(light.rgb(0, 0, 0))
